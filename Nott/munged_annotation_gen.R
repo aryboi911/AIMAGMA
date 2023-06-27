ae3c0e0171c3439afda133cfe47cfac70131d8c0
Aryan Pradhan <42878350+aryboi911@users.noreply.github.com>
2023-06-27 12:59:20 -0400

    Create munged_annotation_gen.R

---
+++ Nott/munged_annotation_gen.R
@@ -0,0 +1,100 @@
+suppressMessages(library(tidyverse))
+suppressMessages(library(magrittr))
+suppressMessages(library(annotatr))
+suppressMessages(library(readxl))
+suppressMessages(library(biomaRt))
+suppressMessages(library(GenomicRanges))
+library(rtracklayer)
+library(dplyr)
+library(AnnotationHub)
+
+## chromosomes for generating granges objects
+chromosome_lengths.hg19 = c(249250621,
+                            243199373,
+                            198022430,
+                            191154276,
+                            180915260,
+                            171115067,
+                            159138663,
+                            146364022,
+                            141213431,
+                            135534747,
+                            135006516,
+                            133851895,
+                            115169878,
+                            107349540,
+                            102531392,
+                            90354753,
+                            81195210,
+                            78077248,
+                            59128983,
+                            63025520,
+                            48129895,
+                            51304566,
+                            155270560,
+                            59373566,
+                            16571) #hg19
+
+chromosome_lengths.hg38 = c(248956422,
+                            242193529,
+                            198295559,
+                            190214555,
+                            181538259,
+                            170805979,
+                            159345973,
+                            145138636,
+                            138394717,
+                            133797422,
+                            135086622,
+                            133275309,
+                            114364328,
+                            107043718,
+                            101991189,
+                            90338345,
+                            83257441,
+                            80373285,
+                            58617616,
+                            64444167,
+                            46709983,
+                            50818468,
+                            156040895,
+                            57227415,
+                            16569) #hg38
+
+names(chromosome_lengths.hg19) = c(seq(1,22), c("X", "Y", "MT")) # seq(1,25)
+names(chromosome_lengths.hg38) = c(seq(1,22), c("X", "Y", "MT")) # seq(1,25)
+
+epig_xlsx = "/sc/arion/projects/load/users/patelt16/projects/aimagma/071422.aimagma/data/placseq_nott_2019/Nott2019_supplement_Table_S5.xlsx"
+snatac_morabito.hg38 = "/sc/arion/projects/load/users/patelt16/projects/aimagma/071422.aimagma/data/snatac_morabito_2021/morabito_peaks_hg38_093021.txt"
+
+
+sumstats_bellenguez.hg19 = read_tsv("/sc/arion/projects/load/users/pradha04/projects/aimagma/abc_magma/data/whiteman/PGCALZ2sumstatsIncluding23andMeMETALNoUKB.chrall.CPRA_b37.tsv")
+
+sumstats_bellenguez.hg19 <- sumstats_bellenguez.hg19[!is.na(sumstats_bellenguez.hg19$POS), ]
+
+## format sumstats for granges input
+sumstats_bellenguez.annot.hg19 = sumstats_bellenguez.hg19 %>% 
+  dplyr::mutate(end = POS+1) %>%
+  dplyr::select(snp=ID, chromosome=CHROM, start=POS, end=end)
+
+## make granges object
+sumstats_bellenguez.annot.hg19 = sumstats_bellenguez.annot.hg19 %>% 
+  GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = TRUE, seqinfo = chromosome_lengths.hg19)
+
+
+nott.promoter.annot <- read_tsv("/sc/arion/projects/load/users/patelt16/projects/aimagma/20220714.aimagma/input/epig_nott2019/nott_2019.microglia.pu1.annot")
+
+nott.promoter.annot.hg19 = nott.promoter.annot %>% 
+  GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = TRUE, seqinfo = chromosome_lengths.hg19)
+
+bellenguez_nott.annot = annotate_regions(sumstats_bellenguez.annot.hg19, nott.promoter.annot.hg19) %>% as_tibble
+bellenguez_nott.annot %<>%
+  dplyr::rename(range_id = annot.range_id, gene_loc = annot.gene_loc) %>%
+  distinct(range_id, gene_loc, snp) %>%
+  arrange(range_id, gene_loc, snp)
+
+bellenguez_nott.annot %<>%
+  group_by(range_id, gene_loc) %>%
+  summarize(snps = str_c(snp, collapse = " "))
+
+unique(bellenguez_nott.annot) %>% write.table(paste0("/sc/arion/projects/load/users/pradha04/projects/aimagma/nott/annotations/wightman/PGCALZ2sumstatsIncluding23andMeMETALNoUKB_chrall.genes.annot"), row.names = FALSE, col.names = FALSE, sep = " ", quote = FALSE)
